681486d99e55d923256344c5023d4410ad10dc91
simonnygren46@gmail.com <simonnygren46@gmail.com>
2025-03-26 12:05:09 +0000

    Edited ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -5,72 +5,12 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def potatis_pressed_enter(self, **event_args
-  # Användaren skriver in en potatissort
-  potatis = input("Vilken potatis har du? ").strip().lower()
-
-    """This method is called when the user presses Enter in this text box"""
-    
-    # Lista med potatisar
-potatis_typer = {
-    'king edward': 'mjölig',
-    'bintje': 'mjölig',
-    'melody': 'fast',
-    'asterix': 'fast',
-    'charlotte': 'fast',
-    'amandine': 'fast',
-    'ratte': 'fast',
-    'connect': 'mjölig',
-    'fontane': 'mjölig',
-    'king edward': 'mjölig',
-    'mandel': 'mjölig',
-    'matilda': 'mjölig',
-    'amadine': 'fast',
-    'anya': 'fast',
-    'apache': 'fast',
-    'asterix': 'fast',
-    'ballerina': 'fast',
-    'belana': 'fast',
-    'cherie': 'fast',
-    'folva': 'fast',
-    'gala': 'fast',
-    'hertha': 'fast',
-    'labella': 'fast',
-    'linzer delikatesse': 'fast',
-    'melody': 'fast',
-    'ratte': 'fast',
-    'sava': 'fast',
-    'sparris': 'fast',
-    'blå Kongo': 'färgad',
-    'cecile': 'färgad',
-    'duble fun': 'färgad',
-    'violet queen': 'färgad',
-    'early puritan': 'färsk',
-    'estelle': 'färsk',
-    'magda': 'färsk',
-    'maria': 'färsk',
-    'marilyn': 'färsk',
-    'rocket': 'färsk',
-    'rosara': 'färsk',
-    'solist': 'färsk',
-    'swift': 'färsk',
-    'färskpotatis': 'färsk',
-    'primörpotatis': 'färsk',
-    'timo': 'fast',
-    'jazzy': 'fast'
-}
-
-
-# Kollar om potatisen finns i listan och skriver ut typen
-if potatis in potatis_typer:
-    print(f"{potatis.capitalize()} är en {potatis_typer[potatis]} potatis.")
-else:
-    print("Antingen har du stavat fel, ditt miffo. Eller så finns inte potatisen i databasen just nu.")
-
-  
+def potatis_pressed_enter(self, **event_args):
+  # Call your 'send_feedback' server function
+  # pass in name, email and feedback as arguments
+  anvil.server.call('send_feedback', potatis).show()
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -3,13 +3,8 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
 
-# This is a server module. It runs on the Anvil server,
-# rather than in the user's browser.
-#
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
+@anvil.server.callable
+def potatis_pressed_enter('input')
 # # Lista med potatisar
 potatis_typer = {
     'king edward': 'mjölig',
@@ -59,11 +54,8 @@ potatis_typer = {
     'jazzy': 'fast'
 }
 
-# Användaren skriver in en potatissort
-potatis = input("Vilken potatis har du? ").strip().lower()
-
 # Kollar om potatisen finns i listan och skriver ut typen
-if potatis in potatis_typer:
-    print(f"{potatis.capitalize()} räknas som en {potatis_typer[potatis]} potatis.")
+if str(potatis) in potatis_typer:
+    anvil.print(f"{potatis.capitalize()} räknas som en {potatis_typer[potatis]} potatis.")
 else:
-    print("Antingen har du stavat fel, ditt miffo. Eller så finns inte potatisen i databasen just nu.")
+    anvil.print("Antingen har du stavat fel, ditt miffo. Eller så finns inte potatisen i databasen just nu.")
