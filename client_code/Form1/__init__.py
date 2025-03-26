f3b48b0e7f20c18efbb8324fa6e9d8318647fe05
simonnygren46@gmail.com <simonnygren46@gmail.com>
2025-03-26 15:20:39 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,15 +4,13 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+import tkinter as tk
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def potatis_pressed_enter(self, **event_args):
+  def potatis_pressed_enter( ):
     """This method is called when the user presses Enter in this text box"""
     pass
-
-def potatis_pressed_enter(self, **event_args):
-  anvil.server.call('send_feedback', potatis).show()
