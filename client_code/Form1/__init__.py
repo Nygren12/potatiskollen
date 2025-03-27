a717a019d3498d2d8b1735265794f4c13f8a443a
simonnygren46@gmail.com <simonnygren46@gmail.com>
2025-03-27 10:20:56 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,13 +4,74 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-import tkinter as tk
+
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-  def potatis_pressed_enter( ):
+
+user_input = self.text_box_1.text.strip().lower()
+x = user_input    
+
+  
+def text_box_1_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
-    pass
+    # Lista med potatisar
+
+x_typer = {
+    'king edward': 'mjölig',
+    'bintje': 'mjölig',
+    'melody': 'fast',
+    'asterix': 'fast',
+    'charlotte': 'fast',
+    'amandine': 'fast',
+    'ratte': 'fast',
+    'connect': 'mjölig',
+    'fontane': 'mjölig',
+    'king edward': 'mjölig',
+    'mandel': 'mjölig',
+    'matilda': 'mjölig',
+    'amadine': 'fast',
+    'anya': 'fast',
+    'apache': 'fast',
+    'asterix': 'fast',
+    'ballerina': 'fast',
+    'belana': 'fast',
+    'cherie': 'fast',
+    'folva': 'fast',
+    'gala': 'fast',
+    'hertha': 'fast',
+    'labella': 'fast',
+    'linzer delikatesse': 'fast',
+    'melody': 'fast',
+    'ratte': 'fast',
+    'sava': 'fast',
+    'sparris': 'fast',
+    'blå Kongo': 'färgad',
+    'cecile': 'färgad',
+    'duble fun': 'färgad',
+    'violet queen': 'färgad',
+    'early puritan': 'färsk',
+    'estelle': 'färsk',
+    'magda': 'färsk',
+    'maria': 'färsk',
+    'marilyn': 'färsk',
+    'rocket': 'färsk',
+    'rosara': 'färsk',
+    'solist': 'färsk',
+    'swift': 'färsk',
+    'färskpotatis': 'färsk',
+    'primörpotatis': 'färsk',
+    'timo': 'fast',
+    'jazzy': 'fast'
+}
+
+# Kollar om potatisen finns i listan och skriver ut typen
+if str(x) in x_typer:
+  print(f"{x.capitalize()} är en {x_typer[x]} potatis.")
+else:
+  print("Antingen har du stavat fel. Eller så finns inte potatisen i databasen just nu.")
+
+
